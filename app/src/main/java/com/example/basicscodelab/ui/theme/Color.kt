0cf38f864903afd696ebc403606931971599fdf8
tsaqifdb <tsaqifworkspace@gmail.com>
2025-03-26 10:30:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.example.basicscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
+
+val CustomPrimary = Color(0xFF6200EE) // Ungu tua
+val CustomSecondary = Color(0xFF03DAC5) // Hijau neon
+val CustomBackground = Color(0xFFF5F5F5) // Abu-abu muda
+val CustomOnBackground = Color(0xFF212121) // Hitam pekat
+
+// Warna untuk mode Light
+val Orange = Color(0xFFFFA726) // Orange
+val White = Color(0xFFFFFFFF) // Putih
+
+// Warna untuk mode Dark
+val GreenArmy = Color(0xFF4B5320) // Green Army
+val GreenSage = Color(0xFF8F9779) // Green Sage
